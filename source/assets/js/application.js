8e9f6087a8874d8f232e3ff6e80acc0e586f7d15
Manu Morante <manuel.morante@razorfishhealthware.com>
2015-06-19 13:17:36 +0200

    Site updated to 4ec1ce3

--- source/assets/js/application.js
+++
@@ -1,8 +0,0 @@
-/**
- * Lib (3rd Party libraries)
- * = require lib/_jquery-1.11.1
- *
- * Modules (common JS in this project)
- * = require ui/app
- * = require ui/music
- */
--- source/assets/js/ui/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Application
- */
-
-$(document).ready(function() {
-  setTimeout(function(){ $("html").addClass("step-1"); }, 2000);
-  setTimeout(function(){ $("html").addClass("step-2"); }, 5000);
-});
