dad0fb0573a50b3b77a81126a83a3d97d237d560
Manu Morante <manu@estadologico.com>
2022-04-04 20:58:48 +0200

    Audio default false

--- src/useAudio.js
+++ src/useAudio.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 export const useAudio = (url) => {
   const [audio] = useState(new Audio(url))
-  const [playing, setPlaying] = useState(true)
+  const [playing, setPlaying] = useState(false)
 
   const toggle = () => setPlaying(!playing)
 
